78fe199bb1cd3e0dac3896705ecb5c8740bac5fa
Flávio Celestino <94621387+FlavioCelestino@users.noreply.github.com>
2022-01-17 20:46:18 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace dotnet // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes=5;
+            for (int i=0; i < numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Bem vindo ao curso de .NET {i}");
+            }
+        }
+    }
+}
